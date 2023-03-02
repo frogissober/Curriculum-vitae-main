9d2e6eecd230ed8490e1ee202f5a5775e17348ad
Floris Bogers <florisbogers@hotmail.nl>
2023-03-02 23:08:19 +0100

    tel fix

--- components/Over.vue
+++ components/Over.vue
@@ -2,7 +2,7 @@
   <div id="over">
     <div class="ik">
         <span class="naam">Floris<span class="oranje">Bogers</span></span> 
-        <span class="adres">Nieuwe Heilaarstraat 37 - 4813AR  BREDA - 06-36493167 - <span class="oranje"><a href="mailto:floris@tonbogers.nl">floris@tonbogers.nl</a></span></span>
+        <span class="adres">Nieuwe Heilaarstraat 37 - 4813AR  BREDA - +31 6 36493167 - <span class="oranje"><a href="mailto:floris@tonbogers.nl">floris@tonbogers.nl</a></span></span>
     </div>
     <div class="content">As a software developer, I am always looking for new challenges. I am eager to learn and enthusiastic about discovering and trying out new programming languages. I am a team player and enjoy working together on projects, but I am also comfortable working independently. In short, I am always ready for whatever the next project has to offer. And who knows, maybe I will discover my new favorite programming language!</div>
     <div class="socials">
